ba8bed7ce7f512d446b927875d8b54fd7b611b03
Sam MacPherson <xylom44@gmail.com>
2022-09-09 13:40:12 -0400

    adding xdomain support

---
+++ src/domains/OptimismDomain.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Copyright (C) 2021 Dai Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+pragma solidity >=0.8.0;
+
+import "./Domain.sol";
+
+contract OptimismDomain is Domain {
+
+    MainnetDomain public immutable mainnet;
+
+    constructor(MainnetDomain _mainnet) Domain("optimism") {
+        mainnet = _mainnet;
+    }
+
+    function relayL1ToL2() external {
+        makeActive();
+    }
+
+    function relayL2ToL1() external {
+        mainnet.makeActive();
+    }
+    
+}
