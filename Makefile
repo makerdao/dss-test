20a26b80ec1ff25c715be4f3c8033660d7a4a883
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2022-12-16 14:24:43 -0700

    update script and test command for multidomain blocks

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 all    :; forge build
 clean  :; forge clean
-test   :; ./test.sh $(match)
+test   :; ./test.sh match=$(match) block="$(block)" opt-block="$(opt-block)" arb-block="$(arb-block)"
--- test.sh
+++ test.sh
@@ -1,8 +1,50 @@
 #!/usr/bin/env bash
 set -e
 
-if [[ -z "$1" ]]; then
-  forge test --rpc-url "$ETH_RPC_URL"
+for ARGUMENT in "$@"
+do
+    KEY=$(echo "$ARGUMENT" | cut -f1 -d=)
+    VALUE=$(echo "$ARGUMENT" | cut -f2 -d=)
+
+    case "$KEY" in
+            match)      MATCH="$VALUE" ;;
+            block)      BLOCK="$VALUE" ;;
+            opt-block)   OPT_BLOCK="$VALUE" ;;
+            arb-block)   ARB_BLOCK="$VALUE" ;;
+            *)
+    esac
+done
+
+# L2 setup
+
+if [[ -z "$OPT_RPC_URL" ]]; then
+    echo "OPTIMISM not used"
+    export OPT_RPC_URL=""
+fi
+
+if [[ -z "$ARB_RPC_URL" ]]; then
+    echo "ARBITRUM not used"
+    export ARB_RPC_URL=""
+fi
+
+if [[ -z "$OPT_BLOCK" ]]; then
+    export OPT_BLOCK=0
 else
-  forge test --rpc-url "$ETH_RPC_URL" --match "$1" -vvv
+    export OPT_BLOCK=$OPT_BLOCK
+fi
+
+if [[ -z "$OPT_BLOCK" ]]; then
+    export ARB_BLOCK=0
+else
+    export ARB_BLOCK=$ARB_BLOCK
+fi
+
+if [[ -z "$MATCH" && -z "$BLOCK" ]]; then
+  forge test --fork-url "$ETH_RPC_URL"
+elif [[ -z "$BLOCK" ]]; then
+  forge test --fork-url "$ETH_RPC_URL" --match "$MATCH" -vvv --force
+elif [[ -z "$MATCH" ]]; then
+  forge test --fork-url "$ETH_RPC_URL" --fork-block-number "$BLOCK" -vvv --force
+else
+  forge test --fork-url "$ETH_RPC_URL" --match "$MATCH" --fork-block-number "$BLOCK" -vvv --force
 fi
