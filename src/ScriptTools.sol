6b7403012148f2bc6cd1c2d71e5e84df77b6053a
Sam MacPherson <xylom44@gmail.com>
2023-01-02 11:23:08 -0500

    Add ScriptTools for common tasks in Scripts and Tests (#16)
    * update forge-std; add ScriptTools to do common tasks required in both scripts and tests
    
    * add importContract and change name of logContract
    
    * add switchOwner helper function
    
    * use a better method to export contracts
    
    * use out folder instead
    
    * add conversion tool to go from ilk to chainlog entry; add some tests for ScriptTools
    
    * add string eq compare
    
    * writing to out dir seems to not be working
    
    * revert last commit

---
+++ src/ScriptTools.sol
@@ -0,0 +1,161 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Copyright (C) 2017 DappHub, LLC
+// Copyright (C) 2022 Dai Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+pragma solidity >=0.8.0;
+
+import { VmSafe } from "forge-std/Vm.sol";
+import { stdJson } from "forge-std/StdJson.sol";
+
+import { WardsAbstract } from "dss-interfaces/Interfaces.sol";
+
+/** 
+ * @title Script Tools
+ * @dev Contains opinionated tools used in scripts.
+ */
+library ScriptTools {
+
+    VmSafe private constant vm = VmSafe(address(uint160(uint256(keccak256("hevm cheat code")))));
+    
+    string internal constant DEFAULT_DELIMITER = ",";
+    string internal constant DELIMITER_OVERRIDE = "DSSTEST_ARRAY_DELIMITER";
+
+    function readInput(string memory input) internal view returns (string memory) {
+        string memory root = vm.projectRoot();
+        string memory chainInputFolder = string(abi.encodePacked("/script/input/", vm.toString(block.chainid), "/"));
+        return vm.readFile(string(abi.encodePacked(root, chainInputFolder, input, ".json")));
+    }
+
+    /**
+     * @notice It's common to define strings as bytes32 (such as for ilks)
+     */
+    function stringToBytes32(string memory source) internal pure returns (bytes32 result) {
+        bytes memory emptyStringTest = bytes(source);
+        if (emptyStringTest.length == 0) {
+            return 0x0;
+        }
+
+        assembly {
+            result := mload(add(source, 32))
+        }
+    }
+
+    /**
+     * @notice Convert an ilk to a chainlog key by replacing all dashes with underscores.
+     *         Ex) Convert "ETH-A" to "ETH_A"
+     */
+    function ilkToChainlogFormat(bytes32 ilk) internal pure returns (string memory) {
+        uint256 len = 0;
+        for (; len < 32; len++) {
+            if (uint8(ilk[len]) == 0x00) break;
+        }
+        bytes memory result = new bytes(len);
+        for (uint256 i = 0; i < len; i++) {
+            uint8 b = uint8(ilk[i]);
+            if (b == 0x2d) result[i] = bytes1(0x5f);
+            else result[i] = bytes1(b);
+        }
+        return string(result);
+    }
+
+    function eq(string memory a, string memory b) internal pure returns (bool) {
+        return keccak256(bytes(a)) == keccak256(bytes(b));
+    }
+
+    /**
+     * @notice Used to export important contracts to higher level deploy scripts.
+     *         Note waiting on Foundry to have better primatives, but roll our own for now.
+     *         Writes contract to out/contract-exports.env
+     */
+    function exportContract(string memory name, address addr) internal {
+        vm.writeLine(string(abi.encodePacked(vm.projectRoot(), "/out/contract-exports.env")), string(abi.encodePacked("export FOUNDRY_EXPORT_", name, "=", vm.toString(addr))));
+    }
+
+    /**
+     * @notice Used to import contracts from previous exports.
+     *         Note waiting on Foundry to have better primatives, but roll our own for now.
+     *         Assume parent script has put environment variables into scope.
+     *         Run `source out/contract-exports.env` in parent script to get environment variables.
+     */
+    function importContract(string memory name) internal view returns (address addr) {
+        return vm.envAddress(string(abi.encodePacked("FOUNDRY_EXPORT_", name)));
+    }
+
+    function switchOwner(address base, address deployer, address newOwner) internal {
+        if (deployer == newOwner) return;
+        require(WardsAbstract(base).wards(deployer) == 1, "deployer-not-authed");
+        WardsAbstract(base).rely(newOwner);
+        WardsAbstract(base).deny(deployer);
+    }
+
+    // Read config variable, but allow for an environment variable override
+
+    function readUint(string memory json, string memory key, string memory envKey) internal returns (uint256) {
+        return vm.envOr(envKey, stdJson.readUint(json, key));
+    }
+
+    function readUintArray(string memory json, string memory key, string memory envKey) internal returns (uint256[] memory) {
+        return vm.envOr(envKey, vm.envOr(DELIMITER_OVERRIDE, DEFAULT_DELIMITER), stdJson.readUintArray(json, key));
+    }
+
+    function readInt(string memory json, string memory key, string memory envKey) internal returns (int256) {
+        return vm.envOr(envKey, stdJson.readInt(json, key));
+    }
+
+    function readIntArray(string memory json, string memory key, string memory envKey) internal returns (int256[] memory) {
+        return vm.envOr(envKey, vm.envOr(DELIMITER_OVERRIDE, DEFAULT_DELIMITER), stdJson.readIntArray(json, key));
+    }
+
+    function readBytes32(string memory json, string memory key, string memory envKey) internal returns (bytes32) {
+        return vm.envOr(envKey, stdJson.readBytes32(json, key));
+    }
+
+    function readBytes32Array(string memory json, string memory key, string memory envKey) internal returns (bytes32[] memory) {
+        return vm.envOr(envKey, vm.envOr(DELIMITER_OVERRIDE, DEFAULT_DELIMITER), stdJson.readBytes32Array(json, key));
+    }
+
+    function readString(string memory json, string memory key, string memory envKey) internal returns (string memory) {
+        return vm.envOr(envKey, stdJson.readString(json, key));
+    }
+
+    function readStringArray(string memory json, string memory key, string memory envKey) internal returns (string[] memory) {
+        return vm.envOr(envKey, vm.envOr(DELIMITER_OVERRIDE, DEFAULT_DELIMITER), stdJson.readStringArray(json, key));
+    }
+
+    function readAddress(string memory json, string memory key, string memory envKey) internal returns (address) {
+        return vm.envOr(envKey, stdJson.readAddress(json, key));
+    }
+
+    function readAddressArray(string memory json, string memory key, string memory envKey) internal returns (address[] memory) {
+        return vm.envOr(envKey, vm.envOr(DELIMITER_OVERRIDE, DEFAULT_DELIMITER), stdJson.readAddressArray(json, key));
+    }
+
+    function readBool(string memory json, string memory key, string memory envKey) internal returns (bool) {
+        return vm.envOr(envKey, stdJson.readBool(json, key));
+    }
+
+    function readBoolArray(string memory json, string memory key, string memory envKey) internal returns (bool[] memory) {
+        return vm.envOr(envKey, vm.envOr(DELIMITER_OVERRIDE, DEFAULT_DELIMITER), stdJson.readBoolArray(json, key));
+    }
+
+    function readBytes(string memory json, string memory key, string memory envKey) internal returns (bytes memory) {
+        return vm.envOr(envKey, stdJson.readBytes(json, key));
+    }
+
+    function readBytesArray(string memory json, string memory key, string memory envKey) internal returns (bytes[] memory) {
+        return vm.envOr(envKey, vm.envOr(DELIMITER_OVERRIDE, DEFAULT_DELIMITER), stdJson.readBytesArray(json, key));
+    }
+
+}
---
+++ src/tests/ScriptToolsTest.t.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Copyright (C) 2022 Dai Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+pragma solidity >=0.8.0;
+
+import "../DSSTest.sol";
+import "../ScriptTools.sol";
+
+contract ScriptToolTest is DSSTest {
+
+    function test_stringToBytes32() public {
+        assertEq(ScriptTools.stringToBytes32("test"),  bytes32("test"));
+    }
+
+    function test_stringToBytes32_empty() public {
+        assertEq(ScriptTools.stringToBytes32(""),  bytes32(""));
+    }
+
+    function test_ilkToChainlogFormat() public {
+        assertEq(ScriptTools.ilkToChainlogFormat(bytes32("ETH-A")), "ETH_A");
+    }
+
+    function test_ilkToChainlogFormat_empty() public {
+        assertEq(ScriptTools.ilkToChainlogFormat(bytes32("")), "");
+    }
+
+    function test_ilkToChainlogFormat_multiple() public {
+        assertEq(ScriptTools.ilkToChainlogFormat(bytes32("DIRECT-AAVEV2-DAI")), "DIRECT_AAVEV2_DAI");
+    }
+
+    function test_eq() public {
+        assertTrue(ScriptTools.eq("A", "A"));
+    }
+
+    function test_not_eq() public {
+        assertTrue(!ScriptTools.eq("A", "B"));
+    }
+
+}
