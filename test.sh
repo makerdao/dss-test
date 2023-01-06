c4ee485a12b9018c5b6e1ab68f68ce6d9ef60d3b
Sam MacPherson <xylom44@gmail.com>
2023-01-06 11:36:33 -0600

    Update test.sh
    Co-authored-by: Chris Smith <1408372+iamchrissmith@users.noreply.github.com>

--- test.sh
+++ test.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+[[ $ETH_RPC_URL  ]] || { echo "Please set an ETH_RPC_URL"; exit 1; }
+
 export FOUNDRY_ROOT_CHAINID="$(cast chain-id)"
 if [ "$FOUNDRY_ROOT_CHAINID" != "1" ] && [ "$FOUNDRY_ROOT_CHAINID" != "5" ]; then
   echo "Invalid chainid of $FOUNDRY_ROOT_CHAINID. Please set your forking environment via ETH_RPC_URL."
   exit
 fi
-
 if [[ -z "$1" ]]; then
   forge test
 else
