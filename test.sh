8a9942738e0ba1f8c2b6393dc1fbffec1b4e8380
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2023-01-09 11:07:29 -0300

    Fix test script indentation + other minors

--- test.sh
+++ test.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env bash
 set -e
 
-if [[ -z "$FOUNDRY_ROOT_CHAINID" ]]; then
-  [[ $ETH_RPC_URL  ]] || { echo "Please set an ETH_RPC_URL"; exit 1; }
-  export FOUNDRY_ROOT_CHAINID="$(cast chain-id)"
-fi
-if [ "$FOUNDRY_ROOT_CHAINID" != "1" ] && [ "$FOUNDRY_ROOT_CHAINID" != "5" ]; then
-  echo "Invalid chainid of $FOUNDRY_ROOT_CHAINID. Please set your forking environment via ETH_RPC_URL or manually by defining FOUNDRY_ROOT_CHAINID."
-  exit
-fi
+[[ -n "$FOUNDRY_ROOT_CHAINID" ]] || {
+    [[ -n $ETH_RPC_URL ]] || {
+        echo "Please set FOUNDRY_ROOT_CHAINID (1 or 5) or ETH_RPC_URL";
+        exit 1;
+    }
+    FOUNDRY_ROOT_CHAINID="$(cast chain-id)"
+}
+[[ "$FOUNDRY_ROOT_CHAINID" == "1" ]] || [[ "$FOUNDRY_ROOT_CHAINID" == "5" ]] || {
+    echo "Invalid chainid of $FOUNDRY_ROOT_CHAINID. Please set your forking environment via ETH_RPC_URL or manually by defining FOUNDRY_ROOT_CHAINID (1 or 5)."
+    exit 1;
+}
 
+[[ "$FOUNDRY_ROOT_CHAINID" == "1" ]] && echo "Running tests on Mainnet"
+[[ "$FOUNDRY_ROOT_CHAINID" == "5" ]] && echo "Running tests on Goerli"
+
+export FOUNDRY_ROOT_CHAINID
 if [[ -z "$1" ]]; then
-  forge test
+    forge test
 else
-  forge test --match "$1" -vvvvv
+    forge test --match "$1" -vvvvv
 fi
