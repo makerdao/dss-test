72b1851953fc2b170ce15283d99c6e318fa336a4
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2023-02-03 17:47:49 -0300

    Use vm.toString and improve comments

--- src/domains/RecordedLogs.sol
+++ src/domains/RecordedLogs.sol
@@ -21,52 +21,33 @@ import { ScriptTools } from "../ScriptTools.sol";
 library RecordedLogs {
     Vm internal constant vm = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
 
-    function _uintToString(uint256 v) private pure returns (string memory str) {
-        if (v == 0) {
-            return "0";
-        }
-        uint256 j = v;
-        uint256 length;
-        while (j != 0) {
-            length++;
-            j /= 10;
-        }
-        bytes memory bstr = new bytes(length);
-        uint256 k = length;
-        j = v;
-        while (j != 0) {
-            bstr[--k] = bytes1(uint8(48 + j % 10));
-            j /= 10;
-        }
-        str = string(bstr);
-    }
-
     function getLogs() internal returns (Vm.Log[] memory) {
-        string memory _logs = vm.serializeUint("LOG", "a", 0);
+        string memory _logs = vm.serializeUint("LOG", "a", 0); // this is the only way to get the logs from the memory object
         uint256 count = ScriptTools.eq(_logs, '{"a":0}') ? 0 : abi.decode(vm.parseJson(_logs, "count"), (uint256));
 
         Vm.Log[] memory newLogs = vm.getRecordedLogs();
         Vm.Log[] memory logs = new Vm.Log[](count + newLogs.length);
         for (uint256 i = 0; i < count; i++) {
-            bytes memory rawData = vm.parseJson(_logs, string(abi.encodePacked(_uintToString(i), "_", "data")));
+            bytes memory rawData = vm.parseJson(_logs, string(abi.encodePacked(vm.toString(i), "_", "data")));
             if (rawData.length > 64) {
-                // We only care about the data field, so we can skip the first 64 bytes
-                // logs that are actually less than 64 bytes are not relevant as message passing
+                // We only need the data content itself, so we have to skip the first 64 bytes
+                // data of logs that are less than a word will not be saved as bytes, so won't have the 64 bytes prefix
+                // as those logs are not bridge message relaying related, we directly ignore their content
                 bytes memory data = new bytes(rawData.length - 64);
                 for (uint256 j = 0; j < data.length; j++) {
                     data[j] = rawData[j + 64];
                 }
                 logs[i].data = data;
             }
-            logs[i].topics  = abi.decode(vm.parseJson(_logs, string(abi.encodePacked(_uintToString(i), "_", "topics"))), (bytes32[]));
-            // emitter is not needed at least for now
-            // logs[i].emitter = abi.decode(vm.parseJson(_logs, string(abi.encodePacked(_uintToString(i), "_", "emitter"))), (address));
+            logs[i].topics  = abi.decode(vm.parseJson(_logs, string(abi.encodePacked(vm.toString(i), "_", "topics"))), (bytes32[]));
+            // emitter is not needed, at least in the actual domains implementations
+            // logs[i].emitter = abi.decode(vm.parseJson(_logs, string(abi.encodePacked(vm.toString(i), "_", "emitter"))), (address));
         }
 
         for (uint256 i = 0; i < newLogs.length; i++) {
-            vm.serializeBytes("LOG", string(abi.encodePacked(_uintToString(count), "_", "data")), logs[count].data = newLogs[i].data);
-            vm.serializeBytes32("LOG", string(abi.encodePacked(_uintToString(count), "_", "topics")), logs[count].topics = newLogs[i].topics);
-            // vm.serializeAddress("LOG", string(abi.encodePacked(_uintToString(count), "_", "emitter")), logs[count].emitter = newLogs[i].emitter);
+            vm.serializeBytes("LOG", string(abi.encodePacked(vm.toString(count), "_", "data")), logs[count].data = newLogs[i].data);
+            vm.serializeBytes32("LOG", string(abi.encodePacked(vm.toString(count), "_", "topics")), logs[count].topics = newLogs[i].topics);
+            // vm.serializeAddress("LOG", string(abi.encodePacked(vm.toString(count), "_", "emitter")), logs[count].emitter = newLogs[i].emitter);
             count++;
         }
         vm.serializeUint("LOG", "count", count);
